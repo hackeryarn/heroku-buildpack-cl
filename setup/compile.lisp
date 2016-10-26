ce525f044dde9103456b59676ce7e34a23546497
Travis <tssund93@gmail.com>
2016-10-26 11:15:37 -0400

    testing something

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -27,24 +27,25 @@
   (if (probe-file ql-setup)
       (load ql-setup)
       (progn
-	(load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
-	(funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
-		 :path (make-pathname :directory (pathname-directory ql-setup))))))
+        (load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
+        (funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
+                 :path (make-pathname :directory (pathname-directory ql-setup))))))
 
 (ecase *cl-webserver*
+  (ningle (ql:quickload "ningle"))
   (hunchentoot (ql:quickload "hunchentoot"))
   (aserve (progn
             (asdf:clear-system "acl-compat")
-	    ;;; Load all .asd files in the repos subdirectory.  The compile script puts
-	    ;;; several systems in there, because we are using versions that are
-	    ;;; different from those in Quicklisp. (update: Can't just load the files apparently,
-	    ;;; have to add dirs to asdf:*central-registry*.  Blah.
-	    (let* ((asds (directory (make-pathname :directory  (append *cache-dir* '( "repos" :wild-inferiors))
-						   :name :wild
-						   :type "asd")))
-		   (directories (remove-duplicates (mapcar #'pathname-directory asds) :test #'equal)))
-	      (dolist (d directories)
-		(push (make-pathname :directory d) asdf:*central-registry*))))))
+      ;;; Load all .asd files in the repos subdirectory.  The compile script puts
+      ;;; several systems in there, because we are using versions that are
+      ;;; different from those in Quicklisp. (update: Can't just load the files apparently,
+      ;;; have to add dirs to asdf:*central-registry*.  Blah.
+            (let* ((asds (directory (make-pathname :directory  (append *cache-dir* '( "repos" :wild-inferiors))
+                                                   :name :wild
+                                                   :type "asd")))
+                   (directories (remove-duplicates (mapcar #'pathname-directory asds) :test #'equal)))
+              (dolist (d directories)
+                (push (make-pathname :directory d) asdf:*central-registry*))))))
 
 ;;; App can redefine this to do runtime initializations
 (defun initialize-application ())
@@ -54,8 +55,10 @@
   (let ((port (parse-integer (heroku-getenv "PORT"))))
     (format t "Listening on port ~A~%" port)
     (ecase *cl-webserver*
+      (ningle (funcall (symbol-function (find-symbol "START" (find-package "NINGLE")))))
+                       ;; (funcall 'make-instance (find-symbol "EASY-ACCEPTOR" (find-package "NINGLE")) :port port)))
       (hunchentoot (funcall (symbol-function (find-symbol "START" (find-package "HUNCHENTOOT")))
-		     (funcall 'make-instance (find-symbol "EASY-ACCEPTOR" (find-package "HUNCHENTOOT")) :port port)))
+                            (funcall 'make-instance (find-symbol "EASY-ACCEPTOR" (find-package "HUNCHENTOOT")) :port port)))
       (aserve (funcall (symbol-function (find-symbol "START" (find-package "NET.ASERVE"))) :port port)))
     (loop (sleep 60))))
 
@@ -64,11 +67,11 @@
 
 (defun h-save-app (app-file)
   #+ccl (save-application app-file
-        :prepend-kernel t
-        :toplevel-function #'heroku-toplevel)
+                          :prepend-kernel t
+                          :toplevel-function #'heroku-toplevel)
   #+sbcl (sb-ext:save-lisp-and-die app-file
-        :toplevel #'heroku-toplevel
-        :executable t))
+                                   :toplevel #'heroku-toplevel
+                                   :executable t))
 
 (let ((app-file (format nil "~A/lispapp" (heroku-getenv "BUILD_DIR")))) ;must match path specified in bin/release
   (format t "Saving to ~A~%" app-file)
